ea617d3d277899ef9e22c963296d042206fef695
Fred Lee <juhee.fred.lee@gmail.com>
2023-11-27 17:04:01 +0900

    Initial commit

---
+++ src/main/java/com/manytooneserverjava/ManytooneServerJavaApplication.java
@@ -0,0 +1,13 @@
+package com.manytooneserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytooneServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytooneServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manytooneserverjava/ManytooneServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manytooneserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytooneServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
