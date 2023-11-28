e115daa473d8eef9564ef18820a68e346f66f2ab
Fred Lee <juhee.fred.lee@gmail.com>
2023-11-28 15:44:10 +0900

    feat: 회원 기본 CRUD 기능 추가
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

---
+++ src/main/java/com/manytooneserverjava/memberrelation/domain/MemberRelationRepository.java
@@ -0,0 +1,6 @@
+package com.manytooneserverjava.memberrelation.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRelationRepository extends JpaRepository<MemberRelation, Long> {
+}
