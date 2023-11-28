dfb51f7c9cd03c10274b60cdd475cafb37f5c945
Fred Lee <juhee.fred.lee@gmail.com>
2023-11-28 15:45:01 +0900

    feat: 마니또 회원 기본 CRUD 기능 추가
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

---
+++ src/main/java/com/manytooneserverjava/manitomember/domain/ManitoMemberRepository.java
@@ -0,0 +1,6 @@
+package com.manytooneserverjava.manitomember.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ManitoMemberRepository extends JpaRepository<ManitoMember, Long> {
+}
