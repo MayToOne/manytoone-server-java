b6488ac7f80e4688fe188f161e92df388bea7710
Fred Lee <juhee.fred.lee@gmail.com>
2023-12-05 15:05:40 +0900

    refactor: 마니또 엔티티 필드 타입 변경 Collection 타입이었던 manitoMembers를 List로 변경
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

--- src/main/java/com/manytooneserverjava/manito/domain/Manito.java
+++ src/main/java/com/manytooneserverjava/manito/domain/Manito.java
@@ -7,6 +7,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -33,7 +34,7 @@ public class Manito extends BaseEntity {
 
     @OneToMany
     @JoinColumn(name = "manito_id")
-    private ArrayList<ManitoMember> manitoMembers = new ArrayList<>();
+    private List<ManitoMember> manitoMembers = new ArrayList<>();
 
     public void updateManito(Boolean updateStatusParam, LocalDateTime updateEndDateTimeParam) {
         this.status = updateStatusParam;
