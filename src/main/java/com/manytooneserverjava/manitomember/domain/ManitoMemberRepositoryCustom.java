339d8daefc24840b470e0f673f80f8626954e9bb
Fred Lee <juhee.fred.lee@gmail.com>
2023-12-05 15:10:40 +0900

    feat, refactor: 마니또 회원 기능 관련 코드 리팩토링 및 기능 추가
    동일 마니또 그룹 내 회원 조회 기능 추가
    마니또, 회원 아이디 기준 마니또 회원 조회 기능 추가
    가입된 마니또 조회 기능 추가
    
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

--- src/main/java/com/manytooneserverjava/manitomember/domain/ManitoMemberRepositoryCustom.java
+++ src/main/java/com/manytooneserverjava/manitomember/domain/ManitoMemberRepositoryCustom.java
@@ -1,9 +1,16 @@
 package com.manytooneserverjava.manitomember.domain;
 
+import com.manytooneserverjava.manito.domain.Manito;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface ManitoMemberRepositoryCustom {
-    List<ManitoMember> findManitoMembersInSameGroup(Long manitoId);
+    List<ManitoMember> findManitoMembersInSameGroup(Long manitoId, Integer status);
 
     List<ManitoMember> findMyManitoInvites(Long memberId);
+
+    Optional<ManitoMember> findByMemberIdAndManitoId(Long memberId, Long manitoId);
+
+    List<Manito> findMyManito(Long memberId);
 }
--- src/main/java/com/manytooneserverjava/manitomember/domain/ManitoMemberRepositoryImpl.java
+++ src/main/java/com/manytooneserverjava/manitomember/domain/ManitoMemberRepositoryImpl.java
@@ -1,26 +1,31 @@
 package com.manytooneserverjava.manitomember.domain;
 
-import com.manytooneserverjava.member.domain.QMember;
+import com.manytooneserverjava.manito.domain.Manito;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import static com.manytooneserverjava.manito.domain.QManito.manito;
 import static com.manytooneserverjava.manitomember.domain.QManitoMember.manitoMember;
 import static com.manytooneserverjava.member.domain.QMember.member;
 
 @RequiredArgsConstructor
 public class ManitoMemberRepositoryImpl implements ManitoMemberRepositoryCustom {
-
     private final JPAQueryFactory query;
 
     @Override
-    public List<ManitoMember> findManitoMembersInSameGroup(Long manitoId) {
+    public List<ManitoMember> findManitoMembersInSameGroup(Long manitoId, Integer status) {
         return query
                 .select(manitoMember)
                 .from(manitoMember)
+                .join(manitoMember.member, member).on(manitoMember.member.id.eq(member.id))
+                .join(manitoMember.manito, manito).on(manitoMember.manito.id.eq(manito.id))
                 .where((manitoMember.manito.id.eq(manitoId))
-                        .and(manitoMember.status.eq(1)))
+                        .and(eqStatus(status)))
                 .fetch();
     }
 
@@ -29,9 +34,44 @@ public class ManitoMemberRepositoryImpl implements ManitoMemberRepositoryCustom 
         return query
                 .select(manitoMember)
                 .from(manitoMember)
-                .join(member)
+                .join(manitoMember.member, member).on(manitoMember.member.id.eq(member.id))
                 .on(manitoMember.member.id.eq(member.id))
                 .where(manitoMember.member.id.eq(memberId))
                 .fetch();
+    }
+
+    @Override
+    public Optional<ManitoMember> findByMemberIdAndManitoId(Long memberId, Long manitoId) {
+        ManitoMember result = query
+                .select(manitoMember)
+                .from(manitoMember)
+                .join(manitoMember.member, member).on(manitoMember.member.id.eq(member.id))
+                .join(manitoMember.manito, manito).on(manitoMember.manito.id.eq(manito.id))
+                .where(
+                        (member.id.eq(memberId))
+                                .and(manito.id.eq(manitoId))
+                                .and(manitoMember.status.eq(1)))
+                .fetchOne();
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public List<Manito> findMyManito(Long memberId) {
+        List<ManitoMember> result = query.select(manitoMember)
+                .from(manitoMember)
+                .join(manitoMember.member, member).on(manitoMember.member.id.eq(member.id))
+                .join(manitoMember.manito, manito).on(manitoMember.manito.id.eq(manito.id))
+                .where(manitoMember.member.id.eq(memberId)
+                        .and(manitoMember.status.eq(1)))
+                .fetch();
+        List<Manito> manitos = result.stream().map(ManitoMember::getManito).collect(Collectors.toList());
+        return manitos;
+    }
+
+    private BooleanExpression eqStatus(Integer status) {
+        if (status == null) {
+            return null;
+        }
+        return manitoMember.status.eq(status);
     }
 }
