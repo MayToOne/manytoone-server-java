656c97c193bb1a8575c4a6b6ed9601b3f53b5ce3
Fred Lee <juhee.fred.lee@gmail.com>
2023-12-07 16:33:51 +0900

    refactor: 예외 처리 코드 수정
    필드값 validation 결과 에러 발생 시 해당 필드:에러내용 추가
    
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

--- src/main/java/com/manytooneserverjava/excpetion/advice/GlobalExceptionHandler.java
+++ src/main/java/com/manytooneserverjava/excpetion/advice/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.manytooneserverjava.excpetion.advice;
 
 import com.manytooneserverjava.excpetion.object.ExceptionResponse;
+import com.manytooneserverjava.excpetion.object.ExceptionResponses;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
@@ -13,7 +18,6 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = IllegalStateException.class)
     public ResponseEntity<ExceptionResponse> illegalStateExceptionHandler(IllegalStateException e) {
-        System.out.println("BAD_REQUEST.getReasonPhrase() = " + BAD_REQUEST.getReasonPhrase());
         return ResponseEntity.badRequest().body(ExceptionResponse.of(BAD_REQUEST.value(), e));
     }
 
@@ -22,4 +26,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(ExceptionResponse.of(BAD_REQUEST.value(), e));
     }
 
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponses> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        List<String> errorMessages = fieldErrors.stream().map(fieldError -> fieldError.getField() + ":" + fieldError.getDefaultMessage()).collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(new ExceptionResponses(BAD_REQUEST.value(), errorMessages));
+    }
 }
