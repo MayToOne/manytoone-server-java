c3a45ea4fdecef217046ba2c526bbe90ba897574
Fred Lee <juhee.fred.lee@gmail.com>
2023-12-07 15:22:51 +0900

    refoctor: 사용하지 않는 코드 제거
    Signed-off-by: Fred Lee <juhee.fred.lee@gmail.com>

--- src/main/java/com/manytooneserverjava/manito/web/ManitoCreateForm.java
+++
@@ -1,8 +0,0 @@
-package com.manytooneserverjava.manito.web;
-
-import jakarta.persistence.Column;
-
-import java.time.LocalDateTime;
-
-public record ManitoCreateForm(Long memberId, String name, LocalDateTime endDateTime) {
-}
